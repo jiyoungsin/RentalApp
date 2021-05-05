18916ebfe14e1a8110eba5941b0efaf56bc16738
Paul Sin <jiyoungsin97@gmail.com>
2021-05-05 16:43:07 -0400

    adding forgot password feature.

--- rental-app-frontend/src/views/Login/login.js
+++ rental-app-frontend/src/views/Login/login.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect}  from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import InputForm from '../../components/InputForm';
+import Button from 'react-bootstrap/Button';
 
 export default function Login() {
     const UseStyles = makeStyles(theme => ({
@@ -17,7 +18,15 @@ export default function Login() {
             width: '60%',
             height: '100vh',
             margin: 'auto',
-        }
+        },
+        buttonPadding: {
+            padding: "10px",
+            marginTop: "20px",
+            marginBottom: "20px",
+        },
+        labelText: {
+            fontSize: '24px',
+        },
     }));
     const classes = UseStyles();
     const [state, setState] = useState({
@@ -34,15 +43,20 @@ export default function Login() {
     }
 
     return (
-        <div className={classes.container}>
-            {/* <h3>{state.name}</h3> */}
-            <form className={classes.formStyle} onSubmit={onSubmit()}>
-                <label for="id">Id</label>
-                <InputForm onChange={handleChange} type="text" value={state.name} placeholder="JohnDoe@gmail.com" id="name" name="name"/>
-                <label for="password">Password</label>
-                <InputForm onChange={handleChange} type="text" value={state.name} placeholder="" id="password" name="password"/>
-                <button>Submit</button>
-            </form>
-        </div>
+        <>
+            <div className={classes.container}>
+                {/* <h3>{state.name}</h3> */}
+                <form className={classes.formStyle} onSubmit={onSubmit()}>
+                    <label className={classes.labelText} for="name">Email</label>
+                    <InputForm onChange={handleChange} type="text" value={state.name} placeholder="JohnDoe@gmail.com" id="name" name="name"/>
+                    <label className={classes.labelText} for="password">Password</label>
+                    <InputForm onChange={handleChange} type="text" value={state.name} placeholder="" id="password" name="password"/>
+                    <Button variant="primary" className={classes.buttonPadding}>Submit</Button>
+                    <div class="container">
+                        <span class="psw">If you have forgotten your password, you can reset it by clicking <a href="/"> Reset Password.</a></span>
+                    </div>
+                </form>
+            </div>
+       </>
     )
 }
