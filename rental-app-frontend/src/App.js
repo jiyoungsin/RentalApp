16dc8e162ee942aca36636948cb7cb0837379920
Paul Sin <jiyoungsin97@gmail.com>
2021-05-07 15:25:17 -0400

    adding dotenv file

--- rental-app-frontend/src/App.js
+++ rental-app-frontend/src/App.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect}  from "react";
 import Routes from './routes/Routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default function App() {
   return (
