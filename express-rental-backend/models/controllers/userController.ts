72c3ba987913e2daaeb4a756731ca37b72179100
Paul Sin <jiyoungsin97@gmail.com>
2021-06-04 02:14:59 -0400

    creation of cookies

--- express-rental-backend/models/controllers/userController.ts
+++ express-rental-backend/models/controllers/userController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
 import  User from '../user.model';
 
 const bcrypt = require('bcrypt');
@@ -13,18 +13,24 @@ export let allUsers = (req: Request, res: Response)=>{
     })
 }
 
-export let find = async (req: Request, res: Response)=>{
+export let find = async (req: Request, res: Response, next: any)=>{
     let user = await User.findOne({email: req.body.email}, (err: any, user: any)=>{ 
         if(err){
             res.send(err);
         }else{
             let passwordAttempt = req.body.password
             if (bcrypt.compareSync(passwordAttempt, user.password)){
-                console.log("Correct Boy wonder")
-                res.status(200)
-                res.send(user);
+                console.log(user.firstName);
+                res.status(200);
+                // req.cookies.isAuth = true;
+                req.cookies.isAdmin = true;
+                req.cookies.isUser = true;
+                req.cookies.user_id = user._id
+                console.log('Cookies: ', req.cookies)
+                return res.send(user);
             }
             else{
+                console.log(err);
                 console.log("Incorrect Password")
             }
         }
