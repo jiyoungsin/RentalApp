53dfb86f86aa61bfa93da06e71e607511697e92f
Asahi <mnuhaque@myseneca.ca>
2021-06-08 14:50:10 -0400

    Added a maintenance view & routed

---
+++ rental-app-frontend/src/views/Maintenance/Maintenance.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+
+export default function Maintenance() {
+ 
+   
+
+   
+
+
+    return (
+        <div>
+            <h1>Niaz was here</h1>
+        </div>
+    )
+}
