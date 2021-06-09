3452959c42017cf12a34fec88c6e8dad14ebf0e7
Asahi <mnuhaque@myseneca.ca>
2021-06-09 03:05:21 -0400

    Added/Implemented Gooogle Maps API, CSS & API CODE refresh need to be done

---
+++ rental-app-frontend/src/views/MapSearch/MapSearch.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import GoogleMap from '../../components/GoogleMap/GoogleMap'
+import { makeStyles } from '@material-ui/core/styles';
+
+export default function MapSearch() {
+  
+
+    return (
+       
+        <div>
+        
+        <GoogleMap />
+     
+        </div>
+    )
+   
+}
