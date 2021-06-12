2b66dcc6f77203afde3437c43c9c8767aa8fb29d
Paul Sin <jiyoungsin97@gmail.com>
2021-06-12 10:49:41 -0400

    working NavBar

--- rental-app-frontend/src/components/NavBar/index.js
+++ rental-app-frontend/src/components/NavBar/index.js
@@ -6,7 +6,42 @@ import {userSessionContext} from "../../contextFile";
 // get the users role to determine navbar
 
 
-function adminNavBar(){
+function adminNavBar(user){
+    const users = user;
+    const profileLink = '/profile/' + users._id
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Link to="/">Vroom</Link>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link><Link to="/findProperty">Find Rental</Link></Nav.Link>
+                    <Nav.Link><Link to="/">Pricing</Link></Nav.Link>
+                    <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                        <NavDropdown.Item><Link to="/">View All Users</Link></NavDropdown.Item>
+                        <NavDropdown.Item><Link to="/">Manage Content</Link></NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="https://cloud.mongodb.com/v2/5f8263f40ca4614ab1bade00#clusters">Database</NavDropdown.Item>
+                        <NavDropdown.Item href="https://github.com/jiyoungsin/RentalApp">Github repo</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="https://www.facebook.com/rentvroom/">Facebook </NavDropdown.Item>
+                        <NavDropdown.Item href="https://www.instagram.com/rentvroom/">Instagram </NavDropdown.Item>
+                        <NavDropdown.Item href="https://twitter.com/Vroomrent">Twitter </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                {/* Linking the routes using Link component */}
+                <Nav>
+                    <Nav.Link><Link to={profileLink} >Profile</Link></Nav.Link>
+                    <Nav.Link><Link to="/logout" >Log out</Link></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+function loggedInUserNavBar(user){
+    const users = user;
+    const profileLink = '/profile/' + users._id
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Link to="/">Vroom</Link>
@@ -14,27 +49,20 @@ function adminNavBar(){
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="/findProperty">Find Rental</Nav.Link>
-                    <Nav.Link href="#bbbbbb">Pricing</Nav.Link>
-                    <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                        <NavDropdown.Item href="#ViewAllUsers">View All Users</NavDropdown.Item>
-                        <NavDropdown.Item href="#ManageContent">Manage Content</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="https://cloud.mongodb.com/v2/5f8263f40ca4614ab1bade00#clusters">Database</NavDropdown.Item>
-                        <NavDropdown.Item href="https://github.com/jiyoungsin/RentalApp">Github repo</NavDropdown.Item>
-                        <NavDropdown.Item href="https://trello.com/b/7yJynBog/rental-app-to-do">Trello Board</NavDropdown.Item>
-                    </NavDropdown>
+                    <Nav.Link href="#Favorites">Favorites</Nav.Link>
+                    <Nav.Link href="#eeeeeeeee">Pricing</Nav.Link>
                 </Nav>
                 {/* Linking the routes using Link component */}
                 <Nav>
-                    <Nav.Link><Link to="/profile">Profile</Link></Nav.Link>
-                    <Nav.Link><Link to="/logout">Log out</Link></Nav.Link>
+                    <Link to={profileLink} style={{'paddingTop': '10px', 'paddingRight': '10px' }}>Profile</Link>
+                    <Link to="/logout" style={{'paddingTop': '10px' }}>Log out</Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
     )
 }
 
-function notLoggedinUserNavBar(){
+function notLoggedInUserNavBar(){
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Link to="/">Vroom</Link>
@@ -53,30 +81,8 @@ function notLoggedinUserNavBar(){
         </Navbar>
     )
 }
-
-function loggedInUserNavBar(){
-    return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Link to="/">Vroom</Link>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/findProperty">Find Rental</Nav.Link>
-                    <Nav.Link href="#Favorites">Favorites</Nav.Link>
-                    <Nav.Link href="#eeeeeeeee">Pricing</Nav.Link>
-                </Nav>
-                {/* Linking the routes using Link component */}
-                <Nav>
-                    <Nav.Link><Link to="/profile">Profile</Link></Nav.Link>
-                    <Nav.Link><Link to="/signout">Sign Out</Link></Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
-}
-
 export default function NavBar() {
     const {user, setUser} = useContext(userSessionContext)
     const role = user.userName;
-    return role === 'sins0113' ? adminNavBar() : role === 'user' ? loggedInUserNavBar() :notLoggedinUserNavBar()
+    return role === 'sins0113' ? adminNavBar(user) : role === 'user' ? loggedInUserNavBar(user) :notLoggedInUserNavBar(user)
 }
