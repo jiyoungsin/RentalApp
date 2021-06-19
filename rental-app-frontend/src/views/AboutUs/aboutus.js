791313ad763af5f60cb3b1f4f2ae8d2766c6c999
Asahi <mnuhaque@myseneca.ca>
2021-06-19 10:46:54 -0400

    Edited the About us info

--- rental-app-frontend/src/views/AboutUs/aboutus.js
+++ rental-app-frontend/src/views/AboutUs/aboutus.js
@@ -8,7 +8,18 @@ export default function AboutUs() {
             <div className="inner-container-about">
                 <h1>About Us</h1>
                 <p className="text-about">
-                We are a group of 5 individuals with the same dream of creating the first digital rental company, Vroom. Vroom’s an application designed to help you during your journey on finding a new rental unit. We all know the struggles finding an optimal home within your budget. Currently in Canada, there are no widely used rental applications to ease the process of finding a home. This results in people using applications such as Facebook marketplace or Kijiji. This app is dedicated to helping renters find a suitable home within their budget and connects landlords to great tenants.
+                We are a group of 5 individuals with the same dream of creating the first digital rental company, Vroom. Vroom’s an application designed to help you during your journey on finding a new rental unit. We all know the struggles finding an optimal home within your budget. Currently in Canada, there are no widely used rental applications to ease the process of finding a home. This results in people using applications such as Facebook marketplace or Kijiji. This app is dedicated to helping renters find a suitable home within their budget and connects landlords to great tenants. 
+                </p>
+                <p className="text-about">
+                Team members - 
+                    <ul>
+                        <li>Mohammed Niaz Ul Haque - Full Stack Developer</li>
+                        <li>EDIT HERE</li>
+                        <li>EDIT HERE</li>
+                        <li>EDIT HERE</li>
+                        <li>EDIT HERE</li>
+                    
+                    </ul>
                 </p>
             </div>
         </div>
